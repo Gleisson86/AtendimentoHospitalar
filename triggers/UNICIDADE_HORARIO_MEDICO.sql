5c4796a347b2a168ba5b8d11d45f7100e6a0346b
philippe <philippe.ruiz.lopes@gmail.com>
2016-07-24 00:32:47 -0300

    Pasta de triggers

---
+++ triggers/UNICIDADE_HORARIO_MEDICO.sql
@@ -0,0 +1,18 @@
+CREATE TRIGGER unicidade_horario_medico
+BEFORE INSERT ON atendimento
+FOR EACH ROW
+DECLARE
+  horarios atendimento.horario%TYPE;
+BEGIN
+  SELECT horario INTO horarios 
+  FROM atendimento 
+  WHERE horario = :new.horario;
+    
+  IF horarios IS NOT NULL THEN
+     RAISE_APPLICATION_ERROR(-20001, 'Horário já agendado');
+  END IF;
+  
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN RETURN;
+    WHEN OTHERS THEN RAISE;
+END;
